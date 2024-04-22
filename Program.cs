6440325918fd87f701897e09fec754d0204b7fef
kael <kael.krause@ucb.edu.bo>
2024-04-22 15:45:49 -0400

    area-de-lote.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hola, bienvenido a:");
+        Console.WriteLine("Calculadora de precio de su lote");
+
+        Console.WriteLine("Ingrese el ancho de su lote por favor:");
+        double ancho = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Ingrese el largo de su lote por favor:");
+        double largo = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Ingrese el precio por metro cuadrado por favor:");
+        double metro = Convert.ToDouble(Console.ReadLine());
+
+        double area = ancho * largo;
+        double precio = area * metro;
+
+        Console.WriteLine($"El área de su lote es de: {area} metros cuadrados");
+        Console.WriteLine($"El precio de su lote es: {precio}$");
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # area-de-lote
 Programa para calcular el metro cuadrado y coste del metro cuadrado
+
+
+Lista de comandos Empleados y pasos seguidos 
+PASOS
+1: Primero creamos un repocitorio en git hub.
+2: seguido  de crear un archivo local donde clonaremos el repocitorio.
+3: seguido de crear un projecto.
+4: seguido de colocar el comadno de code . que nos permite colocar un codigo c# 
+5: seguido luego guardar el projecto ya concluido.
+6: seguido de subirlo todos los cambios a git hub
+COMANDOS EMPLEADOS
+1.- git clone 
+2.- cd "nombre del archivo"
+3.- dotnet new console
+4.- dotnet new -o "nombre del projecto"
+5.- code .
+6.- git add
+7.- git add .
+8.- git commit -m "nombre del projecto"
+9.- git push
